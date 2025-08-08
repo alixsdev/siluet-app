5972fce74d2143dfc0826828f5860386c4979f48
Alissa <alissa@macbookpro.home>
2025-08-09 00:21:00 +0200

    🔧 Ajout bouton Réinitialiser dans la barre d'outils

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -125,6 +125,11 @@ export default function FileUploader() {
     setSelectedIndex(null)
   }
 
+  const resetComposition = () => {
+    setDroppedItems([])
+    setSelectedIndex(null)
+  }
+
   const saveProject = () => {
     const project = {
       id: typeof crypto !== 'undefined' && 'randomUUID' in crypto ? crypto.randomUUID() : `${Date.now()}`,
@@ -315,6 +320,19 @@ export default function FileUploader() {
             <path d="M10 11v6" />
             <path d="M14 11v6" />
             <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2" />
+          </svg>
+        </ToolBtn>
+
+        {/* Réinitialiser (supprimer tous les éléments déposés) */}
+        <ToolBtn
+          title="Réinitialiser"
+          onClick={resetComposition}
+          disabled={droppedItems.length === 0}
+        >
+          {/* Reset icon */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M3 12a9 9 0 1 0 9-9 9 9 0 0 0-6.36 2.64"/>
+            <path d="M3 3v6h6"/>
           </svg>
         </ToolBtn>
 
